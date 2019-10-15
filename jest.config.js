be6c84c4b60761637f2911eb2b6403e717e09cb9
Özüm Eldoğan <ozum@ozum.net>
2019-10-15 18:40:29 +0300

    chore: update development workflow

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-const ignorePatterns = ["<rootDir>/dist/", "<rootDir>/node_modules/", "<rootDir>/test-helper/"];
-
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  coveragePathIgnorePatterns: ignorePatterns,
-  testPathIgnorePatterns: ignorePatterns,
-  coverageThreshold: { global: { branches: 100, functions: 100, lines: 100, statements: 100 } },
-};
